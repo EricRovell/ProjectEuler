bcb6d52a1cbf4286a930722ba694b5dcd7a85feb
Eric Rovell <ericrovell@gmail.com>
2018-10-04 15:01:32 +0300

    problem 009 is solved, brute, works slow, python

---
+++ python/009.brute.py
@@ -0,0 +1,25 @@
+"""
+Problem #009: Special Pythagorean triplet
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+"""
+
+# returns the existing pythagorean triplets that gives the sum "m"
+# a + b + c = m 
+def pythagorean_triplet(m):
+
+  for a in range(1, m):
+    for b in range(1, m):
+      for c in range(1, m):
+        
+        if a + b + c == m:
+          if a ** 2 + b ** 2 == c ** 2:
+            return a * b * c
+
+# test
+print(pythagorean_triplet(1000))
