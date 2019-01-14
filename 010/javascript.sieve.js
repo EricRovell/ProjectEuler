b2c2d2f538c7839493647b39fdc5b0bba89f33b4
Eric Rovell <ericrovell@gmail.com>
2019-01-14 16:10:56 +0300

    010, js solution using the sieve of Eratosthenes

---
+++ 010/javascript.sieve.js
@@ -0,0 +1,33 @@
+// sieve of erathosphenes generator
+function* sieve(limit) {
+  // creating an array where index means actual number
+  // 0 and 1 - are not primes
+  // assuming that all others - are primes
+  let numbers = [0, 0].concat(new Array(limit - 2).fill(1));
+  
+  for (let index = 2; index < numbers.length; index++)
+  {
+    let prime = index;
+    if (numbers[index]) {      
+      yield prime;
+    }
+    // we mark all multiples of the last prime number as non-prime
+    for (let number = index ** 2; number < numbers.length; number += prime)
+    {
+      numbers[number] = 0;
+    }
+  }
+}
+
+// returns a sum of all primes below the given limit
+function primeSum(limit) {
+  let sum = 0;
+  let primes = sieve(limit);
+  for (prime of primes) {
+    sum += prime;
+  }
+  return sum;
+}
+
+// tests
+console.log(primeSum(2000000));
