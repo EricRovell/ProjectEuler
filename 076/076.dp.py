2a0a834d8555c8849cd7d8cb9e3595c0c2de6b8b
Eric Rovell <ericrovell@gmail.com>
2019-04-06 23:04:06 +0300

    076, dynamic programming, python

---
+++ 076/076.dp.py
@@ -0,0 +1,17 @@
+def summations(value: int):
+  ''' Calculates how many different ways can number be written
+  as a sum of at least two positive integers \n
+    Args:
+      value: integer to find number of summations for
+    Returns:
+      number of summations, int '''
+
+  ways = [1] + [0 for _ in range(value)]
+  for number in range(1, value + 1):
+    for addend in range(number, value + 1):
+      ways[addend] += ways[addend - number]
+  # the last element is the answer
+  return ways[-1]
+
+# tests
+# print(summations(100))
