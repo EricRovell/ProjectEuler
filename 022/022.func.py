f1e4600a48f88b4ed738cb872fb0bd561066702f
Eric Rovell <ericrovell@gmail.com>
2019-04-12 21:03:01 +0300

    022. python, added some comments

--- 022/022.func.py
+++ 022/022.func.py
@@ -1,6 +1,7 @@
 _path = '022/data.txt'
 
 def get_data(path: str):
+  ''' Getting data from file. '''
   with open(path) as file:
     file = file.read().split(',')
     return sorted(file)
@@ -8,7 +9,8 @@ def get_data(path: str):
 # ord() -> A-Z -> 65-90
 def scores(path: str, names = get_data(_path)):
   name_score = lambda name: sum(ord(letter) - 64 for letter in name)
+  # name -> sum of digits -> sum times place in the list
   names = map(name_score, names)
   return sum((index + 1) * score for index, score in enumerate(names))
   
-print(scores(_path))
+# print(scores(_path))
