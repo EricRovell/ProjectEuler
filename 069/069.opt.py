ab8055f4979ce30210c48c9149d5283932979760
Eric Rovell <ericrovell@gmail.com>
2019-04-25 18:04:35 +0300

    069, python, small fixes

--- 069/069.opt.py
+++ 069/069.opt.py
@@ -1,7 +1,10 @@
-# The optimized solution
+# The optimized solution.
 # The value of n / phi(n) is only dependent on the number of unique prime factors.
-# (1 - 1 / p) will be minimal for a small p
-# taking the product of the smalles primes is the only resonable thing to do
+# So, to solve the problem, the inversed totient function should be minimized.
+#   Inversed totient function has a (1 - 1 / p) in the denominator.
+#   (1 / p) -> smaller the prime factors, more is subtracted from 1 in...
+#   ... (1 - 1 / p) -> it will be minimal for a small prime factors,
+# then we should be taking the product of the smalles primes to get the answer.
 
 def primes():
   """ Generates prime numbers indefinetly.
@@ -10,12 +13,11 @@ def primes():
     """ Infinite counter generator. """
     while True:
       yield start
-      start += 1
+      start += step
   # yielding 2 as the only even prime number
   # so we can check only odd numbers instead
   yield 2
   for number in infinity():
-    if number % 2 == 0: continue
     for factor in range(3, int(number ** 0.5) + 1, 2):
       if number % factor == 0:
         break
