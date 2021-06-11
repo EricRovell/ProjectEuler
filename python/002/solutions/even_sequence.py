69ca45d8dcf381ff00093950959e68ff91f2125d
Eric Rovell <ericrovell@gmail.com>
2021-06-11 16:25:54 +0300

    python, #002, even sequence solution

---
+++ python/002/solutions/even_sequence.py
@@ -0,0 +1,8 @@
+def fibonacci_sequence_even(limit: int) -> int:
+  a, b = 2, 8
+  while a < limit:
+    yield a
+    a, b = b, 4 * b + a
+    
+def even_fib_sum(limit: int) -> int:
+  return sum([ item for item in fibonacci_sequence_even(limit) if item % 2 == 0 ])
